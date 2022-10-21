74bfa2345e566c371acafd28f2d8f97eeeffa8e2
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-21 13:44:10 -0700

    now hahave the code indented properly. still cant figure out the JS function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-(document).ready function() {
-  ("form#suggestion-form").submit(function(event) {
-    event.preventDefault();
-    const name = $("#name").val();
-    const age = parseInt($("#age").val());
-    const answer1 = $("input:radio[name=question1]:checked").val();
-    const answer2 = $("input:radio[name=question2]:checked").val();
-    const answer3 = $("input:radio[name=question3]:checked").val();
-    const answer4 = $("input:radio[name=question4]:checked").val();
+
+    
+    
+    
+    
+    
+    
+    
     
     if (name.length === 0) {
       alert('Please provide your name.');
@@ -24,3 +23,5 @@
     $(this)[0].reset(); 
   }); 
 });
+
+
