76ee59221f7d4c3c14c15097bac700f642731815
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-21 15:46:30 -0700

    added more styling on the fonts and fonts size. also change the background pickture. been trying to figure out functions in my js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 
+    (document).ready(function(){
+      ("form#suggestion-form").submit(function(event) {
+        event.preventDefault();
+        const name = ("#name").value();
+        const age = parseInt($("#age").val());
+        const answer1 = ("input:radio[name=question1]:checked").value();
+        const answer2 = ("input:radio[name=question2]:checked").value();
+        const answer3 = ("input:radio[name=question3]:checked").value();
+        const answer4 = ("input:radio[name=question4]:checked").value();
+        
+        if (name.length === 0) {
+          alert('Please provide your name.');
+        } else if (!age > 0) {
+          alert('Please provide your age.');
+        } else if (answer1 === "yes") {
+           ("#suggestion1").slideDown();
+        } else if (answer3 === "yes" && answer4 === "yes") {
+           ("#suggestion2").slideDown();
+        } else { 
+           ("#suggestion3").slideDown();
+        };
     
-    
-    
-    
-    
-    
-    
-    
-    if (name.length === 0) {
-      alert('Please provide your name.');
-    } else if (!age > 0) {
-      alert('Please provide your age.');
-    } else if (answer1 === "yes") {
-      $("#suggestion1").slideDown();
-    } else if (answer3 === "yes" && answer4 === "yes") {
-      $("#suggestion2").slideDown();
-    } else { 
-      $("#suggestion3").slideDown();
-    };
-
-    $(".user_name").text(name);
-    $(this)[0].reset(); 
-  }); 
-});
-
-
+        (".user_name").text(name);
+        (this)[0].reset(); 
+      }); 
+    }); 
