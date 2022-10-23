16ee1fef64e3a1a0c81a54d99c0d2c1a3acd43be
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-23 16:46:22 -0700

    now have a better looking js function. still cant get it work properly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 
     
-    function findingPL ();{
-      Event.preventDefault();
-    }
+    function findingPL (); {
+      let from = document.getElementById('#Suggestion-form');
+      onsubmit = function(event) {
+        event.preventDefault();
+      }
         const name = ("#name").value();
         const age = parseInt(("#age").value());
-        const answer1 = document.getElementById('#question1').value();
-        const answer2 = document.getElementById('#question2').value();
-        const answer3 = document.getElementById('#question3').value();
-        const answer4 = document.getElementById('#question4').value();
+        const answer1 = document.querySelector('#question1').value();
+        const answer2 = document.querySelector('#question2').value();
+        const answer3 = document.querySelector('#question3').value();
+        const answer4 = document.querySelector('#question4').value();
 
         
         if (#name === age 0) {
