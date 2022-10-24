5bdeecf3b04a983906bd02689c525ce8efd945c8
BrandonF55 <brandon.fenk99@gmail.com>
2022-10-23 18:29:00 -0700

    now have more JS detail

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 
     
-    function findingPL (); {
+    function findingPL () {
       let from = document.getElementById('#Suggestion-form');
       onsubmit = function(event) {
         event.preventDefault();
-      }
+
         const name = ("#name").value();
         const age = parseInt(("#age").value());
         const answer1 = document.querySelector('#question1').value();
         const answer2 = document.querySelector('#question2').value();
         const answer3 = document.querySelector('#question3').value();
         const answer4 = document.querySelector('#question4').value();
+
+      }
+    }
 
         
         if (#name === age 0) {
